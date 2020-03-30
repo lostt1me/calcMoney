1297fdc4b7b1c4ec567a511efcab67151b803745
Alexander Karavaev <alexander11karavaev@gmail.com>
2020-03-30 21:43:05 +0500

    add font-bold in radio form, cursor-pointer to tooiltip

--- src/Components/PaysForm/View.tsx
+++ src/Components/PaysForm/View.tsx
@@ -21,15 +21,17 @@ export const View = (data: IVeiwProps) => {
     const {hasMrot, netShow, ndflShow, grossShow, changeNet, changeNdflStatus} = data;
     return (
         <form>
-            <div className="d-flex flex-column">
-                <label>Сумма</label>
-                <label><Field name="chargeType" component="input" type="radio" value="salaryMonth"/>Оклад за месяц</label>
-                <div className="d-flex flex-row">
-                    <label><Field name="chargeType" component="input" type="radio" value="mrot"/>МРОТ</label>
-                    <ToolTip />
+            <div className="d-flex flex-column fontForm">
+                <h1>Сумма</h1>
+                <div className="d-flex flex-column radioForm">
+                    <label><Field name="chargeType" component="input" type="radio" value="salaryMonth"/>Оклад за месяц</label>
+                    <div className="d-flex flex-row">
+                        <label><Field name="chargeType" component="input" type="radio" value="mrot"/>МРОТ</label>
+                        <ToolTip />
+                    </div>
+                    <label><Field name="chargeType" component="input" type="radio" value="payDay"/>Оплата за день</label>
+                    <label><Field name="chargeType" component="input" type="radio" value="payHour"/>Оклад за час</label>
                 </div>
-                <label><Field name="chargeType" component="input" type="radio" value="payDay"/>Оплата за день</label>
-                <label><Field name="chargeType" component="input" type="radio" value="payHour"/>Оклад за час</label>
                 {!(hasMrot==="mrot")? <Switch changeStatus={changeNdflStatus}/> : undefined}
                 {!(hasMrot==="mrot")? <MoneyInput payType={hasMrot} changeNet={changeNet}/> : undefined}
                 {(hasMrot==="salaryMonth")? <ShowPay netShow={netShow} ndflShow={ndflShow} grossShow={grossShow}/> : undefined}
