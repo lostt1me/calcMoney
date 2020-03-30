948a958a3a7da60028f95e3d88954bfd63bf8c74
Alexander Karavaev <alexander11karavaev@gmail.com>
2020-03-30 17:14:56 +0500

    change label in switch

--- src/Components/PaysForm/Switch/index.tsx
+++ src/Components/PaysForm/Switch/index.tsx
@@ -12,7 +12,7 @@ export const Switch = (data: ISwitchProps) => {
         <div className="d-flex flex-row">
                     <div className="custom-control custom-switch">
                         <Field name="ndfl" component="input" type="checkbox" className="custom-control-input" id="ndflSwitch" onChange={changeStatus}/>
-                        <label className="custom-control-label-left" htmlFor="ndflSwitch">Учитывать НДФЛ</label>
+                        <label className="custom-control-label-left" htmlFor="ndflSwitch">Указать с НДФЛ</label>
                         <label className="custom-control-label" htmlFor="ndflSwitch">Без НДФЛ</label>
                     </div>
         </div>
