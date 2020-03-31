d40f53a8bb3bdfd0cfc530868d3ab88f9cf6a393
Alexander Karavaev <alexander11karavaev@gmail.com>
2020-03-31 13:11:39 +0500

    add digits of number in ShowPay

--- src/Components/PaysForm/ShowPay/index.tsx
+++ src/Components/PaysForm/ShowPay/index.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import './style.scss';
 import '../MoneyInput/style.scss';
 
-export const ShowPay = (data: any) => {
+interface IShowPayProps {
+    netShow: number;
+    ndflShow: number;
+    grossShow: number;
+}
+export const ShowPay = (data: IShowPayProps) => {
     const { netShow, ndflShow, grossShow } = data;
     return (
         <div className="container-fluid">
             <div className="row">
                 <div className="col-sm-3 show-pay">
                     <div className="d-flex flex-row">
-                        <p><span className="rub">{netShow} i</span> сотрудник будет получать на руки</p>
+                        <p><span className="rub">{netShow.toLocaleString()} i</span> сотрудник будет получать на руки</p>
                     </div>
                     <div className="d-flex flex-row">
-                        <p><span className="rub">{ndflShow} i</span> НДФЛ, 13% от оклада</p>    
+                        <p><span className="rub">{ndflShow.toLocaleString()} i</span> НДФЛ, 13% от оклада</p>    
                     </div>
                     <div className="d-flex flex-row">
-                        <p><span className="rub">{grossShow} i</span> за сотрудника в месяц</p>   
+                        <p><span className="rub">{grossShow.toLocaleString()} i</span> за сотрудника в месяц</p>   
                     </div>
                 </div>
             </div>
